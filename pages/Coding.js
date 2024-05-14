dfbe9a9c1d8aaff55e5c663399af61ed8511c0a7
JOHN <johnaditya46@gmail.com>
2024-05-15 03:37:18 +0530

    changed coding axios post api

--- pages/Coding.js
+++ pages/Coding.js
@@ -9,7 +9,7 @@ function Coding() {
 
   const handleCompile = async (code, lang) => {
     try {
-      const response = await axios.post("http://localhost:3001/compile", {
+      const response = await axios.post("https://e-learning-website-major-project.vercel.app/compile", {
         code,
         lang,
       });
