3fa81b593cabcc900a7a7de1b1f00fd7f5c510aa
JOHN <johnaditya46@gmail.com>
2024-05-15 12:28:33 +0530

    sync

--- pages/Coding.js
+++ pages/Coding.js
@@ -9,7 +9,7 @@ function Coding() {
 
   const handleCompile = async (code, lang) => {
     try {
-      console.log("RESPONSE", response, code, lang);
+      // console.log("RESPONSE", response, code, lang);
       const response = await axios.post(
         "https://e-learning-website-major-project.vercel.app/compile",
         {
