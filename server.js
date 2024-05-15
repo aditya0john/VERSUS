e4b358d491cb3029ba7fbf599dda9b4b8283f154
JOHN <johnaditya46@gmail.com>
2024-05-15 12:35:26 +0530

    sync

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // const { spawn } = require("child_process");
 const cors = require("cors");
 // const readline = require("readline");
