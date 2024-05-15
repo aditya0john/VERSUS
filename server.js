e094947c7ace075aa4152f77016d994d0092a03d
JOHN <johnaditya46@gmail.com>
2024-05-15 09:20:44 +0530

    sync jdoodle

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
 // const { spawn } = require("child_process");
-// const cors = require("cors");
+const cors = require("cors");
 // const readline = require("readline");
 const { default: axios } = require("axios");
 
